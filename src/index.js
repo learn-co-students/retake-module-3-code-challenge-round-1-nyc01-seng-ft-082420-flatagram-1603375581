555788f2c972a0564134a7c03d9cdcf0b3252bea
github username <github email address>
2020-10-22 11:41:44 -0400

    last commit

--- src/index.js
+++ src/index.js
@@ -21,10 +21,9 @@ document.addEventListener('DOMContentLoaded', (e) => {
     imageCard.querySelector('span.likes').textContent = `${image[0].likes}`
     //cant access comments!
     // imageCard.querySelector('ul.comments').textContent= `${image[0].comments}`
-    imageCard.dataset.imageId = `${image.id}`
+    imageCard.dataset.imageId = `${image[0].id}`
    imageContainer.append(imageCard)
 
-   
    
 }
     const clickHandler = () => {
@@ -33,13 +32,56 @@ document.addEventListener('DOMContentLoaded', (e) => {
             if(e.target === 'button.like-button') {
                 const imageId = e.target.dataset.imageId
                 e.target.value++
-                fetch(imageUrl + imageId)
-                .then(response => response.json())
-                .then(console.log())
+                // fetch(imageUrl + imageId)
+                // .then(response => response.json())
+                // .then(console.log())
             }
         })
-
     }
+// These were the next steps but I couldn't increase the likes button :(
+ //PATCH Request for likes:
+
+
+    //const options = {
+        // method: 'PATCH',
+        // headers: {
+        //     "content-type": "application/json",
+        //     "accept": "application/json"
+        // },
+        // body.JSON.stringify()
+    // }
+
+    // const = saveLikes (imageId) => {
+        //fetch(commentUrl + imageId, options)
+        //.then(reponse => reponse.json())
+        //.then(console.log())
+    
+    // }
+    // Comment Post request:
+
+    // const submitHandler = () => {
+    //     const comments = document.querySelector('form.comment-form')
+    //     comments.document.addEventListener('submit', (e) => {
+    //         e.preventDefault()
+    //         const form = e.target
+        
+    //     })
+
+    //const options = {
+        // method: 'POST',
+        // headers: {
+        //     "content-type": "application/json",
+        //     "accept": "application/json"
+        // },
+        // body.JSON.stringify()
+    // }
+
+    // cosnt = createComments () => {
+        //fetch(commentUrl, options)
+        //.then(reponse => reponse.json())
+        //.then(console.log())
+    
+    // }
            
 
     // click event on like button to increase by one and PATCH request for updated likes, POST request for comments
@@ -47,7 +89,7 @@ document.addEventListener('DOMContentLoaded', (e) => {
 
 
 
-
+// submitHandler()
 clickHandler()
 getImage()
 })
