0986292412d4644ec862ba86da09b63ce79bfc1b
aimee-liang <aimeeeliang@gmail.com>
2020-10-22 11:11:03 -0400

    Completed deliverable 2, working on 3

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
-/* Deliverable 3: Add a comment - persistence not needed */
-
 document.addEventListener("DOMContentLoaded", () => {
   /* Deliverable 1: See image from server, when page loads */
   const baseUrl = "http://localhost:3000/images/1"
@@ -19,15 +17,11 @@ document.addEventListener("DOMContentLoaded", () => {
     let comments = document.querySelector(".comments")
     let dogImage = document.querySelector(".image")
 
-    /* set dataset id */
-    // let imageCard = document.querySelector(".image-card")
-    // imageCard.dataset.id = image.id
-
     title.textContent = image.title
     dogImage.src = image.image
     likes.innerHTML = `
-      <span>${image.likes} Likes</span>
-      <button class="like-button" dataset-id=${image.id}>♥</button>
+      <span> ${image.likes} Likes</span>
+      <button class="like-button" data-id=${image.id}>♥</button>
     `
 
     comments.innerHTML = `
@@ -41,14 +35,37 @@ document.addEventListener("DOMContentLoaded", () => {
 increase and will persist */
 
   const clickHandler = () => {
-    const likeButton = document.querySelector(".like-button")
-    likeButton.addEventListener("click", e => {
-      const button = e.target
-      button.dataset.id = image.id
-      console.log()
+    document.addEventListener("click", (e) => {
+      if (e.target.matches("button")){
+        const button = e.target
+        const buttonId = button.dataset.id
+
+        /* select the 0 in likes span, make it an integer, add to this, pass through 
+         options, and then fetch again */
+
+        const spanOfLikes = document.querySelector("span").textContent[1]
+        let parseLikes = parseInt(spanOfLikes)
+        let likesPlusOne = parseLikes + 1
+
+        const options = {
+          method: "PATCH",
+          headers: {
+            "content-type": "application/json",
+            "accept": "application/json"
+          },
+          body: JSON.stringify({likes: likesPlusOne})
+        }
+
+        fetch(baseUrl, options)
+          .then(response => response.json)
+          .then(image => renderImage(image))
+      }
 
     })
   }
+
+/* Deliverable 3: Add a comment - persistence not needed */
+
 
 
   getImages()
